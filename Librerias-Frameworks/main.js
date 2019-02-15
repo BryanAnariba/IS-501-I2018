a15b912a460f4ba9043f60f3fe073c9dac6d258c
Bryan Anariba <saariel115@gmail.com>
2019-02-14 19:20:13 -0600

    commint

--- Librerias-Frameworks/main.js
+++ Librerias-Frameworks/main.js
@@ -2,14 +2,15 @@ $(document).ready(function(){
 	console.log("PROGRAMAR AQUI");
 
 });
+//Para la Ventana oculta
 $('header .wrap #btn-menu').click(function(){
-	if($(".btn-menu span").attr("class")!= "fa fa-bars" ){
+	if($(".btn-menu span").attr("class")!= "fa fa-bars" ){//al dar click cambia el icono de librito a x
 		console.log("Si entro aqui");
 		$(".full-menu").css({"right":"0"});
 
 	}
 });
-$('header .wrap #btn-menu2').click(function(){
+$('header .wrap #btn-menu2').click(function(){//al volver a dar click cambia el icono de x al librito
 	if($(".btn-menu2 span").attr("class")!="fas fa-times"){
 		console.log("Entro aqui");
 		$(".full-menu").css({"right":"-100%"});
